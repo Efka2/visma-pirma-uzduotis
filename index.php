6d48355099120ac5a2730b1601764e2d5d3dc8f5
efka2 <evald.silobrit@ktu.lt>
2021-07-08 14:28:59 +0300

    works with few words

--- index.php
+++ index.php
@@ -19,26 +19,12 @@ else {
 }
 
 $numbers = [0,1,2,3,4,5,6,7,8,9];
-$zero = [0];
-$starting = array();
-$endings = array();
-$answers = array();
-
-
 $wordArray = str_split($word);
 $numberArray = array();
 
-for($i = 0; $i< count($wordArray) * 2 ; $i++){
-    if($i % 2 == 0){
-        $numberArray[$i] = $wordArray[$i/2];
-    }
-    else {
-        $numberArray[$i] = '';
-    }
+for($i = 0; $i< count($wordArray)  ; $i++){
+        $numberArray[$i] = 0;
 }
-
-//$numberArray[0] = '.';
-//$numberArray[count($numberArray)-1] = '.';
 
 foreach ($data as $pattern){
 
@@ -46,142 +32,35 @@ foreach ($data as $pattern){
     $position = strpos( $wordWithDots, $pattern_without_number);
 
     if($position !== false){
-        $patternArray[$position] = $pattern;
-
-//        $pos = $position * 2 - 2;
-//        for($i = 0;$i<= count($patternArray); $i++){
-//            if(is_numeric($patternArray[$i])) $numberArray[$pos] = $patternArray[$i];
-//        }
-//        $numberArray[strlen($pattern)] = filter_var($pattern, FILTER_SANITIZE_NUMBER_INT);
-//
-//        else{
-//            foreach ($patternArray as $key=>$value){
-//                if(is_numeric($value)){
-////                    echo $position * 2 - 1 . "\n";
-//                }
-//            }
-//        }
-//        echo $position * 2 + 1 . "\n";
-//        print_r($patternArray);
-//        foreach ($patternArray as $key => $value){
-//            if(is_numeric($value)){
-//
-//            }
-//        }
-//        break;
+        echo $pattern ."\n";
+        $numberArray = populateNumbersArray($numberArray,$pattern,$position);
     }
 }
-
-
-foreach ($patternArray as $key=>$value){
-    $pattern = str_replace('.', '', $value );
-    $xPattern = str_split($pattern);
-    $chunk = array_chunk($numberArray,count($xPattern)*2,true);
-        print_r($chunk);
-    $x = $key * 2 - 2;
-    if($x <= 0){
-        $x = 0;
+function populateNumbersArray(array $numberArray, string $pattern, int $position): array
+{
+    $patternChars = str_split($pattern);
+    foreach ($patternChars as $char){
+        if(is_numeric($char) && $char >= $numberArray[$position]){
+            $numberArray[$position] = $char;
+        }
+        else $position++;
     }
-//    print_r($xPattern);
-//    echo $x ." $pattern \n";
+    return $numberArray;
 }
 
-//print_r($numberArray);
-
-
-
-//foreach ($patternArray as $key=> $value){
-//    foreach ($numberArray as $key2=>$value2){
-//        if($key * 2 == $key2){
-//            echo strlen($value);
-//            for ($i = 0; $i<strlen($value);$i++){
-//                $a = array_splice($numberArray, $key,strlen($value)+1, $value[$i]);
-//                print_r($a);
-//            }
-////            $numberArray = array_replace($numberArray,$patternArray);
-////            $numberArray[$key]
+print_r($numberArray);
+function remakeWord($numberArray, $word): string{
+    foreach ($numberArray as $key => $value){
+        if($value % 2 !== 0){
+            //todo fix this klaida cia kazkur
+            echo "$key + $value \n";
+            $wordx = substr_replace($word, '-', $key-1, 0);
+//            print_r(str_split($wordx));
 //            break;
-//        }
-//    }
-//    break;
-//}
-//
-//print_r($patternArray);
-//print_r($numberArray);
-
-
-//bandymas su masyvu
-//foreach ($data as $item)
-//{
-//    $pattern_without_number = str_replace($numbers,'',$item);
-//    $position = strpos( $word, $pattern_without_number);
-//
-//    if($position !== false){
-////        echo $position + strlen($item);
-//        $leng = strlen($item);
-//        $itemArray = str_split($item);
-//
-////        echo "postition - " . $position . " leng $leng - $item \n";
-//        foreach ($itemArray as $letter){
-//            if(is_numeric($letter) ){
-////                $answers[] =
-//            }
-//        }
-////        for ($i=0;$i<=count($itemArray);$i++){
-////            echo $itemArray[$i];
-////        }
-//    }
-//}
-//$finalWord = '';
-//foreach ($data as $pattern){
-//    $pattern_without_number = str_replace($numbers,'',$pattern);
-//    $wordWithoutNumbers = str_replace($numbers, '', $word);
-//    $position = strpos( $wordWithoutNumbers, $pattern_without_number);
-//
-//    if($position !== false){
-//        $answers[$position] = $pattern;
-//        $word = str$pattern;
-//        echo $word;
-//        break;
-//    }
-//}
-
-//foreach ($endings as $item){
-//
-//    $numbersWithDot = [0,1,2,3,4,5,6,7,8,9,'.'];
-//    $item2 = trim(str_replace($numbersWithDot,'',$item));
-//
-//    if(strpos(substr($word,strlen($word) - strlen($item2)), $item2) !== false) {
-//        $answers[strpos($word,$item2)] = $item;
-//    }
-//}
-//
-//foreach ($starting as $item){
-//
-//    $numbersWithDot = [0,1,2,3,4,5,6,7,8,9,'.'];
-//    $item2 = trim(str_replace($numbersWithDot,'',$item));
-//
-//    if(strpos(substr($word,0), $item2) !== false && strpos(substr($word,0,), $item2) === 0){
-//        $answers[strpos($word,$item2)] = $item;
-//    }
-//}
-//
-//ksort($answers);
-//$finalWord = implode('',$answers);
-//print_r($answers);
-
-//remakeWord();
-
-/*
-Nurodytam faile esančios reikšmės susideda iš kelių simbolių ir skaičių.
-
-• Įvestame žodyje turi būti rasti atitikmenys iš faile esančių reikšmių.
-• Reikšmė .mis1 žodyje mistranslate atitinka žodžio pradžią (taškas reiškia pradžią, o skaičius įterpiamas po raidės).
-• Reikšmė a2n žodyje mistranslate atitinka bet kurią vietą žodyje.
-• Radus visus atitikmenis žodis turi būti perdarytas įterpiant skaičius į atitinkamas vietas tarp raidžių.
-  Jei skaičių vietos kartojasi turi būti įterpiamas didžiausias.
-• Perdarius žodį naudojant reikšmes gausime naują reikšmę kur nelyginiai skaičiai atitinka skiemens galą.
-• Mistranslate žodį perdarius į .m2is1t4ra2n2s3l2a4te.
-                                 0123456789    ir pakeitus nelyginius skaičius brūkšniu, gausime - mis-trans-late
-
-*/
+        }
+    }
+    return $wordx;
+}
+$finalWord = remakeWord($numberArray,$word);
+echo $finalWord ."\n";
+//echo "$wordWithDots";
--- print.php
+++ print.php
@@ -1,14 +1,2 @@
 <?php
 
-
-function remakeWord(){
-    $numbers = [0,1,2,3,4,5,6,7,8,9];
-    $oddNumbers = [1,3,5,7];
-
-    $a = 'm2is1t4ra2n2s3l2a4te';
-
-    $a = str_replace($oddNumbers, '-',$a);
-    $a = str_replace($numbers, '',$a);
-
-    echo $a . "\n";
-}
