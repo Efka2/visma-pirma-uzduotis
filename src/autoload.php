112c387f7440e49f7ffbf9dd0b1ab284152c1e39
Efka2 <evald.silobrit@ktu.edu>
2021-07-13 09:28:23 +0300

    Add psr-4 autoloader

--- src/autoload.php
+++ src/autoload.php
@@ -1,5 +1,15 @@
 <?php
 
-spl_autoload_register(function($className){
-    include_once str_replace("\\",'/',$className) . ".php";
-});
+//spl_autoload_register(function($className){
+//    require_once str_replace("\\",'/',$className) . ".php";
+//});
+
+//full path classes
+spl_autoload_register(
+    function ($class) {
+        $file = __DIR__.'/'.str_replace('\\', '/', $class).'.php';
+        if (file_exists($file)) {
+            require_once $file;
+        }
+    }
+);
